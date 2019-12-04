cec6e83cb34687cc926f03b159d679634e4e4d8e
Alex Shillingford <alex.shillingford@icloud.com>
2019-12-03 22:48:42 -0700

    Add background and zombie properties, add background and zombie to scene

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -10,7 +10,22 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    // MARK: - Properties
+    let background = SKSpriteNode(imageNamed: "background1")
+    let zombie = SKSpriteNode(imageNamed: "zombie1")
+    
+    // MARK: - Methods
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
+        background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+//        background.zRotation = CGFloat.pi / 8
+        background.zPosition = -1
+        addChild(background)
+        let mySize = background.size
+        print("Size: \(mySize)")
+        
+        zombie.position = CGPoint(x: 400, y: 400)
+        addChild(zombie)
     }
 }
